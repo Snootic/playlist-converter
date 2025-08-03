f5d849d9afe5331fa0de45feb5d8b0c68d811ebe
Snootic <snooticonoob@hotmail.com>
2025-08-03 00:37:10 -0300

    feat: implement convert route for playlist conversion

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import authRoutes from '@/api/routes/auth';
+import convertRoutes from '@/api/routes/convert';
 
 const router = Router();
 
 router.use('/authenticate', authRoutes);
+
+router.use('/convert', convertRoutes);
 
 export default router;
---
+++ server/src/api/routes/convert.ts
@@ -0,0 +1,63 @@
+import { SpotifyService } from '@/services/spotify';
+import { YoutubeService } from '@/services/youtube';
+import { getMetadata } from '@/utils/detectSource';
+import { AccessToken } from '@spotify/web-api-ts-sdk';
+import { Request, Response, Router } from 'express';
+
+const router = Router();
+
+router.post('/', async (req: Request, res: Response) => {
+  const origin = req.body.origin
+  const originToken = req.body.origin_token
+  const destination = req.body.destination
+  let destinationTokenData = req.body.destination_token_data
+  const playlistURl = req.body.playlist_url
+
+  let clientDisconnected = false;
+  res.on('close', () => {
+    clientDisconnected = true;
+    res.end();
+  });
+
+  let playlistItems;
+  let originClass;
+  let destinationClass;
+
+  destinationTokenData =  JSON.parse(destinationTokenData);
+
+  switch (origin) {
+    case 'YouTube':
+      originClass = new YoutubeService(originToken);
+      playlistItems = await originClass.getPlaylistTracks(playlistURl);
+  }
+  
+  if (!playlistItems) {
+    throw new Error("Playlist empty or not found")
+  }
+
+  switch (destination) {
+    case 'Spotify':
+      const spotifyToken: AccessToken = {
+        access_token: destinationTokenData.access_token,
+        token_type: destinationTokenData.token_type,
+        expires_in: destinationTokenData.expires_in,
+        expires: destinationTokenData.expires,
+        refresh_token: destinationTokenData.refresh_token
+      };
+      
+      destinationClass = new SpotifyService(spotifyToken)
+  }
+
+  if (!destinationClass) {
+    throw new Error("Destination service not initialized");
+  }
+
+  for (let item of playlistItems) {
+    if (clientDisconnected) break;
+    item = await getMetadata(item, originClass);
+    const result = await destinationClass.convert(item);
+    res.write(`${JSON.stringify(result)}`);
+  }
+})
+
+export default router
